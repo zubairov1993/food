232eec9d56fe7b18a24b8bb6eb742ecf7dc40b1a
vadim <vadim.zubairov1993@gmail.com>
2020-07-10 11:25:37 +0500

    'axios'

--- js/script.js
+++ js/script.js
@@ -193,37 +193,51 @@ document.addEventListener("DOMContentLoaded", () => {
     return await res.json(); 
   };
 
+
+  //--------------------------------------------------------------------------
   // getResource("http://localhost:3000/menu")
   //   .then(data => {
   //     data.forEach(({img, altimg, title, descr, price}) => {
   //       new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
   //     });
   //   });
-  getResource('http://localhost:3000/menu')
-    .then(data => createCard(data));
+  //-------------------------------------------------------------------------
 
-  function createCard(data) {
-    data.forEach(({img, altimg, title, descr, price}) => {
-      const element = document.createElement('div');
-
-      price *= 27;
-
-      element.classList.add('menu__item');
-
-      element.innerHTML = `
-        <img src=${img} alt=${altimg}>
-        <h3 class="menu__item-subtitle">${title}</h3>
-        <div class="menu__item-descr">${descr}</div>
-        <div class="menu__item-divider"></div>
-        <div class="menu__item-price">
-          <div class="menu__item-cost">Цена:</div>
-          <div class="menu__item-total"><span>${price}</span> грн/день</div>
-        </.div>
-      `;
-
-      document.querySelector('.menu .container').append(element);
+    axios.get("http://localhost:3000/menu")
+      .then(data => {
+        data.data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
     });
-  }
+
+  //------------------------------------------------------------------------
+  // getResource('http://localhost:3000/menu')
+  //   .then(data => createCard(data));
+
+  // function createCard(data) {
+  //   data.forEach(({img, altimg, title, descr, price}) => {
+  //     const element = document.createElement('div');
+
+  //     price *= 27;
+
+  //     element.classList.add('menu__item');
+
+  //     element.innerHTML = `
+  //       <img src=${img} alt=${altimg}>
+  //       <h3 class="menu__item-subtitle">${title}</h3>
+  //       <div class="menu__item-descr">${descr}</div>
+  //       <div class="menu__item-divider"></div>
+  //       <div class="menu__item-price">
+  //         <div class="menu__item-cost">Цена:</div>
+  //         <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //       </.div>
+  //     `;
+
+  //     document.querySelector('.menu .container').append(element);
+  //   });
+  // }
+  //-------------------------------------------------------------------------
+
 
   //Forms
 
